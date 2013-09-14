c90ed3ecc63a8820ab7e080acfbf220a5fa4da28
illuz <iilluzen@gmail.com>
2013-09-14 21:09:08 +0800

    Some Old Data

---
+++ python/a.py
@@ -0,0 +1,6 @@
+def favorite_actors(*s):
+    """Prints out your favorite actorS (plural!)"""
+    print "Your favorite actors are:" , s
+    
+    
+favorite_actors("Michael Palin", "John Cleese", "Graham Chapman")
