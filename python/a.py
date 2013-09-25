d2cf24fbb057f9a471598cb60da184ca159befbc
illuz <iilluzen@gmail.com>
2013-09-26 01:02:48 +0800

    delete some binary file

--- python/a.py
+++
@@ -1,6 +0,0 @@
-def favorite_actors(*s):
-    """Prints out your favorite actorS (plural!)"""
-    print "Your favorite actors are:" , s
-    
-    
-favorite_actors("Michael Palin", "John Cleese", "Graham Chapman")
