20fcbe2146f1da0c9faeab22a1b922239edde982
hanzheng <iilluzen@gmail.com>
2015-09-07 19:04:16 +0800

    Add env_change_list.sh for record and test.

---
+++ profile backup/env_change_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# hehe.sh created by illuz at 2015-09-07 19:00:04
+# This file is just record work environment and test github
+
+echo - `date` >> env_change_list.sh
+echo \ `uname -a` >> env_change_list.sh
+exit 0
+
+# The follow is record list
+- Mon Sep 7 19:03:40 CST 2015
+ Darwin mogujiedeMacBook-Pro-7.local 14.3.0 Darwin Kernel Version 14.3.0: Mon Mar 23 11:59:05 PDT 2015; root:xnu-2782.20.48~5/RELEASE_X86_64 x86_64
